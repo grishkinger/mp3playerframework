c10c037777cde260b4b4b0cfb69f42a5f0660d66
Grish Kinger <grishkinger2008@gmail.com>
2025-06-11 09:39:03 -0400

    Add files via upload

---
+++ processinstaller.py
@@ -0,0 +1,12 @@
+import subprocess
+import sys
+import time
+
+def package_installer(package):
+    subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
+    time.sleep(5) #if your system can install this quicker, change the value. 
+    #this is developed on an i3 from 2016, so yes, it takes this long.
+package = "pygame","tk","matplotlib","mutagen"
+
+for package in package:
+    package_installer(package)
