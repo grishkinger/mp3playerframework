71378c3c598ded8e415d963db95f45b8fbca462b
Grish Kinger <grishkinger2008@gmail.com>
2025-06-13 01:41:52 -0400

    Update processinstaller.py

--- processinstaller.py
+++ processinstaller.py
@@ -6,7 +6,7 @@ def package_installer(package):
     subprocess.check_call([sys.executable, '-m', 'pip', 'install', package])
     time.sleep(5) #if your system can install this quicker, change the value. 
     #this is developed on an i3 from 2016, so yes, it takes this long.
-package = "pygame","tk","matplotlib","mutagen"
+package = "pygame","tk","matplotlib","mutagen,pyqt5"
 
 for package in package:
     package_installer(package)
